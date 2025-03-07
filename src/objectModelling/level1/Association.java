7cc914eb6f6b432a47ca9d95ccd378108cf29b7e
githarshitajj <harshitajangra1703@gmail.com>
2025-03-07 15:47:37 +0530

    completed assissted problems in object modelling

---
+++ src/objectModelling/level1/Association.java
@@ -0,0 +1,10 @@
+package objectModelling.level1;
+class Bank{
+
+}
+class Customer{
+
+}
+public class Association {
+
+}
---
+++ src/objectModelling/level1/Composition.java
@@ -0,0 +1,15 @@
+package objectModelling.level1;
+class Company{
+
+}
+class Department{
+
+}
+class Employee{
+
+}
+public class Composition {
+    public static void main(String[] args) {
+
+    }
+}
